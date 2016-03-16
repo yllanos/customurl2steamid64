a5838b5317a9de5aab1bc7b057439bdf9f0aed7c
Yamil Llanos <yamil.llanos@gmail.com>
2016-03-15 20:54:29 -0500

    Update test.js
    Adding some code comments for JSDoc. Streamlining URL variable

--- test.js
+++ test.js
@@ -2,8 +2,15 @@ var getSteamID64 = require('./lib/steamid64');
 
 var username = 'yllanos';
 var baseURL = "http://steamcommunity.com/id/";
+var URL = baseURL + username +  "/?xml=1";
 
-getSteamID64(baseURL + username +  "/?xml=1")
+/**
+ * Test function that requires main script and logs to console some sample SteamID64
+ * If you want to test a different user, just edit the 'username' variable as needed
+ * 'result' variable represents the value to be integrated into your application
+ * @param {string} URL - Full URL profile for specific user.
+ */
+getSteamID64(URL)
   .then(function (result) {
     console.log(result);
   })
